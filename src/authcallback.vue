1374097ff16fa15488cabd1a7e6200a1d9c40364
Roman Yerchyk <lann.wauthority@gmail.com>
2026-03-02 12:08:04 +0200

    Woah

--- src/authcallback.vue
+++ src/authcallback.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
 import { onMounted } from 'vue';
+import { useAuthStore } from '@/storage/auth.ts';
 
 const route = useRoute();
 const router = useRouter();
+const auth = useAuthStore();
 
 onMounted(async () => {
   const code = route.query.code;
@@ -16,6 +18,8 @@ onMounted(async () => {
   const data = await res.json();
   localStorage.setItem('access_token', data.access_token);
   localStorage.setItem('refresh_token', data.refresh_token);
+
+  await auth.login();
 
   router.push('/');
 });
